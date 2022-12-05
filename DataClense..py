e4a9d120715999da22384ee2c3f465d2da0b8e5c
Yuvaraj N <56192888+Yuvaraj2519@users.noreply.github.com>
2022-12-05 13:41:18 +0000

    Initial commit
    added required files

---
+++ DataClense..py
@@ -0,0 +1,20 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+data = pd.read_csv("data.csv");
+
+print (data.head())
+
+#before cleansing the data
+sns.lineplot(data=data, x="Item", y="Units")
+plt.show()
+
+cleansed_data = data.groupby('Item').sum().reset_index()
+print (cleansed_data)
+
+#After cleanse
+cleansed_data.to_csv("cleaned_data.csv",index=False)
+sns.lineplot(data=cleansed_data, x="Item", y="Units")
+plt.show()
